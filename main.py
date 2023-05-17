1f25b79e045b4c757302e914d9b34fabd53bc165
tomaszenk0 <122213650+tomaszenk0@users.noreply.github.com>
2023-05-17 18:53:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,103 +1,89 @@
 import numpy as np
-#inicjalizacja tablicy
-a=np.array([0,1])
-print(a)
-#lub drugi sposób
-a=np.arange(2,5,0.1)
-print(a)
-#wypisanie typu zmiennej tablicy(nie jej elementow)
-print(type(a))
-#sprawdzenie typu danych tablicy
-print(a.dtype)
-#inicjalizacja tablicy z konketnym typem danych
-a=np.arange(2, dtype='int64')
-print(a.dtype)
-#zapisywanie kopii tablicy jako tablicy z innym typem
-b=a.astype('float')
-print(b)
-print(b.dtype)
-#zapisywanie rozmiaru tablicy
-print(b.shape)
-#można też sprawdzić ilość wymiarów tablicy
-print(a.ndim)
-#stworzenie tablicy wielowymiarowej może wyglądać tak
-#paramentrem przekazywanym do funkcji array jest obiekt,
-#może to być pythonowa lista
-m=np.array([np.arange(2), np.arange(2)])
-print(m.shape)
-#ponownie typem jest ndarray
-print(type(m))
-
-zera=np.zeros((5, 5))
-jedynki=np.ones((4, 4))
-print(zera)
-print(jedynki)
-#warto sprawdzić jaki jest domyślny typ danych takich
-print(zera.dtype)
-print(jedynki.dtype)
-#można również stworzyć pustą macierz o podanym wymiarze
-#wartości umieszczone są losowe, najpierw podawana jest
-pusta=np.empty((2, 2))
-print(pusta)
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 #
-macierz=np.array(((12, 11), (2, 1)))
-print(macierz)
-#do elementów tablicy możemy odwołać się tak jak do elementów
-poz_1=macierz[1, 1]
-poz_2=macierz[0][1]
-print(poz_1)
-print(poz_2)
+# ts=pd.Series(np.random.randn(1000))
+# ts=ts.cumsum()
+#
+# df=pd.DataFrame(ts, columns=['wartości'])
+# print(df)
+# df['średnia krocząca']=df.rolling(window=50).mean()
+# df.plot()
+# plt.legend()
+# plt.show()
+#
+# x=np.random.randn(1000)
+# plt.hist(x, bins=50, facecolor='g', alpha=0.75, density=True)
+# plt.xlabel('Wartości')
+# plt.ylabel('Prawdopodobieństwo')
+# plt.title('histogam')
+# plt.grid()
+# plt.show()
+#
+#
+#
+#
+# df=pd.read_csv('dane.csv', header=0, sep=";", decimal=".")
+# print(df)
+# seria=df.groupby('Imię i nazwisko')['Wartość zamówienia'].sum()
+# wedges,texts, autotext=plt.pie(x=seria, labels=seria.index,
+#                                autopct=lambda pct: "{:.1f}%".format(pct),
+#                                textprops=dict(color="black"),
+#                                colors=['red', 'green'])
+# plt.title('suma zamówień dla sprzedawców')
+# plt.legend(loc='lower right')
+# plt.ylabel('procentowy wynik zamowienia')
+# plt.show()
+#
+#
+
+sns.set()
+# sns.lineplot(x=[1,2,3,4], y=[1,4,9,16], label='linia nr',
+#              color='red',marker='o', linestyle=':')
+# sns.lineplot(x=[1, 2, 3, 4], y=[2, 5, 10 ,17], label='linia nr',
+#              color='green', marker='o', linestyle=':')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.show()
 
 
-#podobnie działa funckja linspace które działanie jest
-liczby_lin=np.linspace(1, 2, 5, endpoint=False)
-print(liczby_lin)
-#a teraz możemy utworzyć dwie macierze najpierw..
-z=np.indices((5, 3))
-print(z)
-print(z[0][1][2])
-#podobnie jak w Matlab możemy tworzyć macierze
-mat_diag=np.diag([a for a in range(5)])
-print(mat_diag)
 
-znaki=b'ogolna'
-z1=np.frombuffer(znaki, dtype='S1')
-print(z1)
-z2=np.frombuffer(znaki, dtype='S2')
-print(z2)
+# s=pd.Series(np.random.randn(1000))
+# s=s.cumsum()
+# wykres=sns.relplot(kind='line', data=s, label='dane z serii')
+# wykres.fig.set_size_inches(8, 6)
+# wykres.fig.suptitle('wykres liniowy')
+# wykres.set_xlabels('indeksy')
+# wykres.set_ylabels('wartosci')
+# wykres.add_legend()
+# #wykres.figure.subplots_adjust(left=0.1, right=0.9,
+# #                             bottom=0.1, top=0.9)
+# plt.show()
+#
+#
+# data={'a':np.arange(10),
+#       'c':np.random.randint(0,50, 10),
+#       'd':np.random.randn(10)}
+# data['b']=data['a']+10*np.random.randn(10)
+# data['d']=np.abs(data['d'])*100
+# df=pd.DataFrame(data)
+# plot=sns.relplot(data=df, x='a', y='b', hue='c',
+#                  palette='bright', size='d', legend=True)
+# plot.set(xticks=data['a'])
+# plt.show()
 
-znaki='ogolna'
-z3=np.array(list(znaki))
-z4=np.array(list(znaki), dtype='S1')
-z5=np.array(list(b'ogolna'))
-z6=np.fromiter(znaki, dtype='S1')
-print(z3)
-print(z4)
-print(z5)
-print(z6)
 
-a=np.arange(10)
-print(a)
-s=slice(2, 7, 2)
-print(a[s])
 
-print(a[2:7:2])
 
-print(a[1:])
-print(a[2:5])
-
-mat=np.arange(25)
-
-mat=mat.reshape((5, 5))
-print(mat)
-print(mat[1:])
-print(mat[:, -1])
-print(mat[2:5, 1:3])
-print(mat[:, [2, 4]])
-print('')
-x=np.array([0, 1, 2], [3, 4, 5], [6, 7, 8])
-print(x)
-rows=np.array([[0, 0], [3, 3]])
-cols=np.array([[0, 2], [0, 2]])
-y=x[rows, cols]
-print(y)
+data={'Kraj':['Belgia', 'Indie', 'Brazylia', 'Polska'],
+      'Stolica':['Bruksela', 'New Delhi', 'Brasilia', 'Warszawa'],
+      'Kontynent':['Europa', 'Azja', 'Ameryka Południowa', 'Europa'],
+      'Populacja':[11190846, 1303171035, 207847528, 38675467]}
+df=pd.DataFrame(data)
+plot=sns.barplot(data=df, x='Kontynent', y='Populacja',
+                hue='Kontynent', estimator=np.sum, errorbar=None,
+                 dodge=False, palette=['red', 'green', 'blue'])
+plot.legend(title='populacja na kontynencie')
+plot.set(title='wykres słupkowy')
+plt.show()
